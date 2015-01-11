03b45862fec7abea1fa8e197a69cc009e7a2d690
Sina Mashek <mashek@thescoundrels.net>
2015-01-11 12:25:31 -0800

    Changed excess references from scrib to warpserver.

--- docs/make.bat
+++ docs/make.bat
@@ -115,9 +115,9 @@ if "%1" == "qthelp" (
 	echo.
 	echo.Build finished; now you can run "qcollectiongenerator" with the ^
 .qhcp project file in %BUILDDIR%/qthelp, like this:
-	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\scrib.qhcp
+	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\warpserver.qhcp
 	echo.To view the help file:
-	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\scrib.ghc
+	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\warpserver.ghc
 	goto end
 )
 
